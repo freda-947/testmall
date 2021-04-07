f920692a6847f20ff29f4707040fc398112d5511
freda <82009664+freda-947@users.noreply.github.com>
2021-04-07 10:31:00 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+   <main-tab-bar/>
+  </div>
+</template>
+
+  <script>
+// import MainTabBar from 'components/content/maintabbar/MainTabBar.vue'
+// import MainTabBar from 'components/MainTabBar.vue'
+import MainTabBar from 'components/content/MainTabBar'
+
+  export default {
+    name:'app',
+    components: {
+      MainTabBar
+
+    },
+  }
+  </script>
+<style>
+@import 'assets/css/base.css';
+</style>
